fe4d4afa489a22aa1dd3d00b516e6680c5c23991
ssubedi2046 <ssubedi2046@gmail.com>
2020-03-29 13:41:00 -0500

    Add files via upload

---
+++ gradledemo/src/main/java/gradledemo/HelloFromGradle.java
@@ -0,0 +1,17 @@
+package gradledemo;
+
+public class HelloFromGradle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		    	System.out.println("Hello from gradle project");
+		    	for(int i=0;i<100;i++) {
+		    		
+		    		System.out.println("Hello-" +i);
+		    	}
+		     
+		   
+	}
+
+}
